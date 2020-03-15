28bc794e258031116cc97838c2f4ede0f5ab3301
Philip Gressman <ptgressman@gmail.com>
2020-03-14 20:14:36 -0400

    daily update

--- data_analyze.py
+++ data_analyze.py
@@ -7,7 +7,7 @@ CSVout = "smoothed.csv"
 
 locations = [['Mainland China'],['Delaware County','PA','US'],['PA','US'],[]]
 
-remove = 4
+remove = 2
 
 def locator_to_label(locator):
     if len(locator) == 0:
