1f6a723f1da2429572755ee17fab489c5fd2de62
Philip Gressman <ptgressman@gmail.com>
2020-03-25 15:50:43 -0400

    save less stuff

--- archiveconfig.py
+++ archiveconfig.py
@@ -4,28 +4,28 @@
 'archive' : 'archiveCDC/web', 'checktimes' : [3], 'extension' : '.html'},
 
 { 'url' : 'https://www.cdc.gov/coronavirus/2019-ncov/map-cases-us.json',
-'archive' : 'archiveCDC/states', 'checktimes' : [0,1,2,3], 'extension' : '.json'},
+'archive' : 'archiveCDC/states', 'checktimes' : [1,3], 'extension' : '.json'},
 
 { 'url' : 'https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/total-cases-onset.json',
-'archive' : 'archiveCDC/cases', 'checktimes' : [0,1,2,3], 'extension' : '.json'},
+'archive' : 'archiveCDC/cases', 'checktimes' : [1,3], 'extension' : '.json'},
 
 { 'url' : 'https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/us-cases-epi-chart.json',
-'archive' : 'archiveCDC/byons', 'checktimes' : [0,1,2,3], 'extension' : '.json'},
+'archive' : 'archiveCDC/byons', 'checktimes' : [1,3], 'extension' : '.json'},
 
 { 'url' : 'https://www.livescience.com/coronavirus-updates-united-states.html',
 'archive' : 'archiveOTHER/LiveSci', 'checktimes' : [0,2], 'extension' : '.html'},
 
 { 'url' : 'https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html',
-'archive' : 'archiveNYT/', 'checktimes' : [0,1,2,3], 'extension' : '.html'},
+'archive' : 'archiveNYT/', 'checktimes' : [1,3], 'extension' : '.html'},
 
 { 'url' : 'https://www.worldometers.info/coronavirus/',
 'archive' : 'archiveOTHER/worldomet', 'checktimes' : [1,3], 'extension' : '.html'},
 
 { 'url' : 'https://en.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic',
-'archive' : 'archiveOTHER/wikiped', 'checktimes' : [0,1,2,3], 'extension' : '.html'},
+'archive' : 'archiveOTHER/wikiped', 'checktimes' : [0,2], 'extension' : '.html'},
 
 { 'url' : 'https://coronavirus.1point3acres.com',
-'archive' : 'archiveOTHER/opta', 'checktimes' : [0,1,2,3], 'extension' : '.html'},
+'archive' : 'archiveOTHER/opta', 'checktimes' : [0,2], 'extension' : '.html'},
 # I get a 403 Forbidden error for some reason
 
 { 'url' : 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports/',
